43cbba2bd88367a4100f6dfb88149b24fcd7a443
VijayaprakashVp <prakashvijay905@gmail.com>
2022-04-01 19:03:09 +0530

    update

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const NewsController = require("./controllers/news.controller");
-// const LatestPhotoController = require("./controllers/latest_photos.controller");
+const newsController = require("./controllers/news.controller");
+const latestPhotoController = require("./controllers/latest_photos.controller");
 const feedController = require("./controllers/feed.controller");
-// const MiniFeedController = require("./controllers/minifeed.controller");
+const miniFeedController = require("./controllers/minifeed.controller");
 // const ScheduleController = require("./controllers/schedule.controller");
 // const VideosController = require("./controllers/videos.controller");
 // const PhotosController = require("./controllers/photos.controller");
@@ -20,9 +20,9 @@ const feedController = require("./controllers/feed.controller");
 // });
 
 app.use("/feed", feedController);
-// app.use("/news", NewsController);
-// app.use("/latestphoto", LatestPhotoController);
-// app.use("/minifeed", MiniFeedController);
+app.use("/news", newsController);
+app.use("/latestphoto", latestPhotoController);
+app.use("/minifeed", miniFeedController);
 // app.use("/schedule", ScheduleController);
 // app.use("/videos", VideosController);
 // app.use("/photos", PhotosController);
